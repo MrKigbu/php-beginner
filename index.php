80d56dc4cb96511a9c588f73b64c1cfa52cc2412
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-10 23:53:54 +0100

    php default value

--- index.php
+++ index.php
@@ -14,34 +14,15 @@ ini_set('display_startup_errors', 1);
     <h1>I LOVE CODING PERSONALLY</h1>
 
     <?php
-   //loops
-   //while loops
-   $digit = 1;
-   while ($digit <= 3)
-   {
-    echo "The digit is " .  $digit. "<br>";
-    $digit++;
-   }
-//do while loop
+//PHP default value
+ function addNumber($num1, $num2){
+    return $num1 + $num2;
+ }
+ define("name", "Samuel bololo");
+ define("last", "Kilishi");
+ echo 71 + addNumber(52, 52);
+ 
 
-$x = 1;
-
-do {
-    echo "The number is " .$x. "<br>";
-    $x++;
-} while ($x<=5);
-
-//for loop
-for($x = 0; $x <=10; $x++ )
-{
-    echo "The whole complete Number is $x<br>";
-}
-
-//for each loop, its initiated only with arrays 
-$cars = array("Blue car", "red car", "green car", "Purple car");
-foreach($cars as $key=> $value) {
-    echo "This is the $value with the number $key <br>";
-}
     ?>
 </body>
 </html>
