f75e1a59582bf9fdbfe73bcfb8385ae7618c1f93
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 19:52:16 +0100

    File_put_Content

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-//delete file
-$filename ="uploads/Sammy faceShot.jpg";
-if(unlink($filename)){
-    echo "file Deleted";
-}else {
-    echo "Unable to delete file";
+//File_put_Content
+$content = "<h1> I'M SURE YOU KNOW HOW WE DO IT</h1>" ;
+//file will be created, and if the file does exist, it will override the file 
+if(file_put_contents("./about.html", $content, FILE_APPEND)){
+    echo "File APPENDED";
 }
 ?>
