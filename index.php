0f80dc5b89b4f8d3a26f7cbfd970fbf2f751b014
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 19:39:12 +0100

    delete file

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP file upload</title>
-</head>
-<body>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <h3>Select File to upload</h3>
-        <input type="file" name="fileimage">
-        <input type="submit" value="Upload File">
-    </form>
-</body>
-</html>
+<?php
+//delete file
+$filename ="uploads/Sammy faceShot.jpg";
+if(unlink($filename)){
+    echo "file Deleted";
+}else {
+    echo "Unable to delete file";
+}
+?>
