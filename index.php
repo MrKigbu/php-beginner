96c49b76f9d546e732c76834419f5fed9e95ee69
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 13:31:41 +0100

    Get a time

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php
-// Enable full error display
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//time H for 24hrs 
+date_default_timezone_set("Africa/Lagos");
+echo date("H")."<br>";
+//time h for 12hrs 
+echo date("h")."<br>";
+//time i for minutes  
+echo date("i")."<br>";
+//time s for seconds 
+echo date("s")."<br>";
+//time a for lowercase Ante meridien 
+echo date("a")."<br>";
+
+//using everything together 
+echo "The time is " .date("H:h:i:s:a");
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Sammy Love in the PHP building</title>
-</head>
-<body>
-    <h1>I LOVE CODING PERSONALLY</h1>
-
-    <section>
-        <footer>
-            <p>
-                //copyright at current date
-                &copy; <?php echo date("Y"); ?> Sammy Project
-            </p>
-        </footer>
-    </section>
-    
-    //prints date
-    <?php
-    echo "Today's date is " . date("l d_m_Y");
-    ?>
-</body>
-</html>
