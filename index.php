415420e16f066e03d3c3d62f1d6d2dbb1e00bf2e
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 14:02:47 +0100

    Date from string

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
-//time H for 24hrs 
+//Convert String to Time
 date_default_timezone_set("Africa/Lagos");
-echo date("H")."<br>";
-//time h for 12hrs 
-echo date("h")."<br>";
-//time i for minutes  
-echo date("i")."<br>";
-//time s for seconds 
-echo date("s")."<br>";
-//time a for lowercase Ante meridien 
-echo date("a")."<br>";
 
-//using everything together 
-echo "The time is " .date("H:h:i:s:a");
+$t = strtotime("10:30pm November 21 2025");
+echo "The new time is ".date("Y-m-d h:s:i:a", $t) ;
+
+// echo date("H")."<br>";
+// //time h for 12hrs 
+// echo date("h")."<br>";
+// //time i for minutes  
+// echo date("i")."<br>";
+// //time s for seconds 
+// echo date("s")."<br>";
+// //time a for lowercase Ante meridien 
+// echo date("a")."<br>";
+
+// //using everything together 
+// echo "The time is " .date("H:h:i:s:a");
 ?>
