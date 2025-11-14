15e9eabf1b112543ff74920b2d3af569c8e7f6c1
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 22:36:12 +0100

    External form submission

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<?php
-//how to do validation with PHP
-$old_password = "123456";
-if(isset($_POST["login"])){
-    //password validate 
-    if($old_password == $_POST["password"]){
-         $name = $_POST["name"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    echo "Welcome ooo $name, Your email is $email, and also your password is $password, thank you very much";
 
-    echo "<b style='color:primary'>Login Successful</b>";
-    }else{
-        echo "<b style='color:red'>Login failed</b>";
-    }
-    //declare the variable
-   
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,7 +9,7 @@ if(isset($_POST["login"])){
 <body>
     <h2>Submit Form </h2>
    
-    <form action="" method="POST">
+    <form action="process.php" method="POST">
         <label for="">Name:</label>
         <input type="text" name="name" id=""><br>
         <label for="">Email:</label>
