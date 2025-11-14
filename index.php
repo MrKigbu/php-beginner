f1b5c396338e4768286ced6d9c5da9d688445825
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 20:04:51 +0100

    File_get_Content

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-//File_put_Content
-$content = "<h1> I'M SURE YOU KNOW HOW WE DO IT</h1>" ;
-//file will be created, and if the file does exist, it will override the file 
-if(file_put_contents("./about.html", $content, FILE_APPEND)){
-    echo "File APPENDED";
-}
+//File_Get_Content
+$file = file_get_contents("./about.html");
+echo $file;
 ?>
