6ff38f1fd017474157ec1147d0b3470e5d015a07
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 22:29:48 +0100

    Password Validation

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
-//how to assess your data from a POST Method
+//how to do validation with PHP
+$old_password = "123456";
 if(isset($_POST["login"])){
-    //declare the variable
-    $name = $_POST["name"];
+    //password validate 
+    if($old_password == $_POST["password"]){
+         $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
     echo "Welcome ooo $name, Your email is $email, and also your password is $password, thank you very much";
 
+    echo "<b style='color:primary'>Login Successful</b>";
+    }else{
+        echo "<b style='color:red'>Login failed</b>";
+    }
+    //declare the variable
+   
 }
 ?>
 <!DOCTYPE html>
