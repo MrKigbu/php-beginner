191cb4a781ec287eaff19898f5ed40ab02d9096d
samuelkigbu <kigbusamuel5@gmail.com>
2025-11-14 23:14:37 +0100

    Splitting pages an using the require

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
+<?php require "inc/header.php";?>
+<h1>Hello, world!</h1>
+<?php require "inc/footer.php";?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Submit Form </h2>
-   
-    <form action="process.php" method="POST">
-        <label for="">Name:</label>
-        <input type="text" name="name" id=""><br>
-        <label for="">Email:</label>
-        <input type="email" name="email" id=""> <br>
-        <label for="">password:</label>
-        <input type="password" name="password" id="">
-        <button type="submit" name="login" >login</button>
-    
-</body>
-</html>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-//session destroy AKA logout.
-session_start();
-session_destroy();
-echo "sessin destroyed";
-
-
-?>
